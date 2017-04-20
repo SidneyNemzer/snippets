7239f15ad6ea873cd0a66e7fa75e0ab397e155f5
SidneyNemzer <sidney@ucode.com>
2017-04-19 22:18:42 -0700

    Add webpack config (not used)
    This file is included just in case I switch from gulp to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,9 @@ const path = require('path')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, './src'),
-  entry: 'panel.js',
+  entry: path.resolve(__dirname, 'src/panel.js'),
   output: {
-    path: path.resolve(__dirname, './build'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'panel.min.js'
   },
   plugins: [
