becdfd57bf7d513d7931342d36fd63f71916fbe0
SidneyNemzer <sidney@ucode.com>
2017-04-19 21:44:14 -0700

    Add some test code

--- src/panel.js
+++ src/panel.js
@@ -4,6 +4,12 @@ function evalInWindow(string) {
   chrome.devtools.inspectedWindow.eval(string)
 }
 
+class PanelConsole {
+  log(input) {
+    evalInWindow('console.log(' + input + ')')
+  }
+}
 
+const logger = new Logger('panel', new PanelConsole())
 
-const logger = new Logger('panel')
+logger.log('Testing')
