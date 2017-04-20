675dc758b72e811a76faaa67af126485b3ee3d94
SidneyNemzer <sidney@ucode.com>
2017-04-19 20:40:42 -0700

    Add uglifyjs plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path')
 
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
   context: path.resolve(__dirname, './src'),
   entry: 'panel.js',
   output: {
     path: path.resolve(__dirname, './build'),
     filename: 'panel.min.js'
-  }
+  },
+  plugins: [
+    new UglifyJSPlugin()
+  ]
 }
