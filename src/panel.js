e8c7a6004d0337052e27dc9434942d5f10eaa6a8
SidneyNemzer <sidney@ucode.com>
2017-04-20 18:13:48 -0700

    Update

--- src/panel.js
+++ src/panel.js
@@ -4,11 +4,6 @@ function evalInWindow(string) {
   chrome.devtools.inspectedWindow.eval(string)
 }
 
-console.log('(native console) hello from panel')
-
-evalInWindow('console.log("hello from panel")')
-
-try {
 class PanelConsole {
   log(input) {
     evalInWindow('console.log("' + input + '")')
@@ -17,10 +12,4 @@ class PanelConsole {
 
 const logger = new Logger('panel', new PanelConsole())
 
-logger.log('Testing')
-} catch(e) {
-  evalInWindow('console.log("Error in panel :(")')
-  const err = JSON.stringify(e)
-  evalInWindow(`console.log("${err}")`)
-  console.log(e)
-}
+logger.log('sub b')
