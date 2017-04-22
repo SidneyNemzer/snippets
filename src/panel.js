2f72cf82bd618d6d8ee600e07724e7cae3ab960f
SidneyNemzer <sidney@ucode.com>
2017-04-21 18:47:40 -0700

    Start converting to react

--- src/panel.js
+++ src/panel.js
@@ -1,4 +1,10 @@
 import Logger from './logger'
+import React from 'react'
+import { render } from 'react-dom'
+import brace from 'brace'
+import AceEditor from 'react-ace'
+
+import 'brace/mode/javascript'
 
 function evalInWindow(string) {
   chrome.devtools.inspectedWindow.eval(string)
@@ -15,7 +21,15 @@ const logger = new Logger('panel', new PanelConsole())
 logger.log('Initializing...')
 
 try {
-ace.edit('editor')
+
+  render(
+    <AceEditor
+      mode="javascript"
+      onChange={newValue => logger.log(newValue)}
+      name="editor"
+    />,
+    document.getElementById('root')
+  )
 
 } catch (exception) {
   // TODO Switch to logger.error when error is implemented in logger
