191f50e93261b5833613af84b643ebf06cfe059a
SidneyNemzer <sidney@ucode.com>
2017-04-25 17:47:11 -0700

    Add class name

--- src/components/Sidepane.js
+++ src/components/Sidepane.js
@@ -6,7 +6,7 @@ import SnippetList from './SnippetList'
 class Sidepane extends React.Component {
   render() {
     return (
-      <div>
+      <div className="sidepane">
         <CreateSnippetButton />
         <SnippetList />
       </div>
