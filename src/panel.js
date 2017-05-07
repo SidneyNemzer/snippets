1b2722c1bf234b77086c3fbb279656e40d3da096
SidneyNemzer <sidney@ucode.com>
2017-05-06 18:39:32 -0700

    Cleanup

--- src/panel.js
+++ src/panel.js
@@ -1,47 +1,17 @@
-import Logger from 'logger'
-
 // React is not explicitly used in this file,
 // but JSX compiles to React.createElement calls
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
-// TODO This function probably can be removed
-function evalInWindow(string) {
-  chrome.devtools.inspectedWindow.eval(string)
-}
-
-const logger = new Logger('panel')
-logger.debug('Initializing...')
-
-const snippets = {
-  1: {
-    name: 'snippet 1',
-    content: 'console.log(\'hi\')'
-  },
-  2: {
-    name: 'snippet 2',
-    content: 'function test() {\n\talert(\'This is a test\')\n}'
-  },
-  3: {
-    name: 'snippet 3',
-    content: 'hi'
-  }
-}
-
 try {
-  ReactDOM.render(
-    <App
-      snippets={snippets}
-      selectedSnippet={'1'}
-    />,
-    document.getElementById('root')
-  )
+  ReactDOM.render(<App/>, document.getElementById('root'))
 } catch (error) {
+  // TODO Better error display
   const rootElement = document.getElementById('root')
   rootElement.innerHTML = '<h1>Critical Error!</h1>' +
                           '<p>Sorry, an error occurred :(</p>' +
                           '<p>' + error + '</p>'
-  console.error('Critical error!')
+  console.error('Snippets: critical error!')
   console.error(error)
 }
