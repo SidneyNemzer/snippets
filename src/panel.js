a72d933dee6303b8466d3bbf809f6a49d06d1308
SidneyNemzer <sidney@ucode.com>
2017-05-10 18:37:15 -0700

    Begin refactoring storage managment

--- src/panel.js
+++ src/panel.js
@@ -4,6 +4,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+/**
+ * Save data to Chrome's Sync storage
+ * @param  {string} key        A key, which is used to reference this data in the storage
+ * @param  {*} value           A value to store
+ * @param  {boolean} mergeValue If `value` is an object, setting this to true will keep keys
+ *                              from the original value that are unchaned in the new value
+ * @return {undefined}
+ */
+function saveToStorage(key, value, mergeValue) {
+  return new Promise(function (resolve, reject) {
+    chrome.storage.sync.get(null, function (storage) {
+      const previousValue = storage[key]
+    })
+  })
+}
+
+/**
+ * Retrieves data from Chrome's sync storage
+ * @param  {string} key The key which references 
+ * @return {[type]}     [description]
+ */
+function loadFromStorage(key) {
+
+}
+
 try {
   ReactDOM.render(<App/>, document.getElementById('root'))
 } catch (error) {
