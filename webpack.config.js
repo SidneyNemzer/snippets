993e31f1b3df80391ae8f5e1bb5132c6592f213c
SidneyNemzer <sidney@ucode.com>
2017-07-07 23:22:08 -0700

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const pages = require('./src/build')
 
+// Prevents deprecation warnings
 process.noDeprecation = true
 
-function relativePath(subURL) {
-  return path.resolve(__dirname, subURL)
-}
+const config = {
+  entry: {},
+  output: {},
 
-module.exports = {
-  entry: relativePath('src/panel.js'),
-  output: {
-    path: relativePath('build'),
-    filename: 'panel.min.js'
-  },
-  devtool: 'inline-source-map',
+  plugins: [],
+
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         include: [
-          relativePath('src')
+          './src'
         ],
         use: {
           loader: 'babel-loader',
@@ -31,14 +29,44 @@ module.exports = {
         test: /\.css/,
         use: [
           'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          }
+          'css-loader'
         ]
       }
     ]
+  },
+
+  devServer: {
+    contentBase: './build',
+    inline: true,
+    stats: {
+      colors: true
+    }
   }
 }
+
+const generateConfig = (baseConfig, pagesToAdd) => {
+  baseConfig.entry = pagesToAdd.reduce(
+    (accumulator, page) => (Object.assign(accumulator, {
+      [page.output.bundle]: './src/' + page.input.entryScript
+    })),
+    {}
+  )
+
+  baseConfig.output = {
+    path: path.resolve('./build/'),
+    filename: '[name].js'
+  }
+
+  baseConfig.plugins = pagesToAdd.map(page => (
+    new HtmlWebpackPlugin({
+      title: page.options.title,
+      filename: page.output.html,
+      template: 'src/' + page.input.template,
+      chunks: [page.output.bundle]
+    })
+  ))
+
+  return baseConfig
+}
+
+module.exports = generateConfig(config, pages)
