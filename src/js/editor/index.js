a8048c07f089e6c169aaf99abd10e147cdc3c0b6
SidneyNemzer <sidney@ucode.com>
2017-08-12 11:59:27 -0700

    Move store creation out of index

--- src/js/editor/index.js
+++ src/js/editor/index.js
@@ -1,49 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './components/App'
 import ErrorPage from './components/ErrorPage'
 
-import createSnippetStore from './stores/snippets'
-
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
-// Generate a unique ID using the crypto API
-// Source: https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-function uuidv4() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  )
-}
-
-export default (saveToStorage, loadFromStorage) => {
-  loadFromStorage('snippets')
-    .then(initalSnippets => {
-      const handleSnippetsChange = (id) => {
-        console.info('snippet changed: ', id)
-      }
-
-      const snippetStore = createSnippetStore(initalSnippets)
-      snippetStore.subscribe(handleSnippetsChange)
-
+export default (store) => {
       try {
         ReactDOM.render(
-          <App
-            snippetStore={snippetStore}
-            getNextId={uuidv4}
-          />,
+					<Provider store={store}>
+          	<App/>
+					</Provider>,
           document.getElementById('root')
         )
       } catch (error) {
@@ -59,5 +25,5 @@ export default (saveToStorage, loadFromStorage) => {
         )
         console.error(error)
       }
-    })
+    //})
 }
