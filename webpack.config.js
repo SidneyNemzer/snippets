bf4bd57ddc67fb9afd068842983a37df4cd3285f
SidneyNemzer <sidney@ucode.com>
2017-08-12 17:04:32 -0700

    Fix loading of font files

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ const config = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
   },
