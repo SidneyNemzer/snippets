8d7f7fa5d01e950bd37317bb26c8055dbfcce570
SidneyNemzer <sidney@ucode.com>
2017-08-16 23:29:02 -0700

    Implement basic settings transition

--- src/js/editor/components/App.js
+++ src/js/editor/components/App.js
@@ -1,26 +1,66 @@
 import React from 'react'
 
+import { CSSTransitionGroup } from 'react-transition-group'
 import Main from './Main'
+import Settings from './Settings'
 
 import 'typeface-roboto'
 import '../../../style/main.css'
+
+const views = {
+  MAIN: 'MAIN',
+  SETTINGS: 'SETTINGS'
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      view: views.MAIN
+    }
 
+    this.handleOpenSettings = this.handleOpenSettings.bind(this)
+  }
+
+  handleOpenSettings() {
+    this.setState({
+      view: views.SETTINGS
+    })
+  }
+
+  renderCurrentView() {
+    switch (this.state.view) {
+      case views.MAIN:
+        return (
+          <Main
+            {...this.props}
+            key={views.MAIN}
+            handleOpenSettings={this.handleOpenSettings}
+          />
+        )
+
+      case views.SETTINGS:
+        return (
+          <Settings
+            key={views.SETTINGS}
+          />
+        )
+
+      default:
+        throw new Error('Unknown view')
     }
   }
 
   render() {
     return (
-      <div>
-        <Main
-          {...this.props}
-        />
-      </div>
+      <CSSTransitionGroup
+        transitionName="view-transition"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}
+      >
+        {this.renderCurrentView()}
+      </CSSTransitionGroup>
     )
   }
 }
---
+++ src/js/editor/components/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <div className="settings">
+        Setings
+      </div>
+    )
+  }
+}
+
+export default Settings
