7da8da9f2c98a76ce1a4660d5e1740a77148e37b
SidneyNemzer <sidney@ucode.com>
2017-08-17 23:14:22 -0700

    Begin creating settings page

--- src/js/editor/components/Settings.js
+++ src/js/editor/components/Settings.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import IconButton from 'material-ui/IconButton'
+import ArrowBackIcon from 'material-ui-icons/ArrowBack'
+import Button from 'material-ui/Button'
+
 class Settings extends React.Component {
   render() {
     return (
       <div className="settings">
-        Setings
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              onClick={this.props.handleCloseSettings}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+            Settings
+          </Toolbar>
+        </AppBar>
       </div>
     )
   }
