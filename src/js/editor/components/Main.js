5cc0d50a34ff57c75980b3b202ffa405f463d00b
SidneyNemzer <sidney@ucode.com>
2017-08-19 15:23:57 -0700

    Cleanup

--- src/js/editor/components/Main.js
+++ src/js/editor/components/Main.js
@@ -12,8 +12,6 @@ import Sidepane from './Sidepane'
 import Header from './Header'
 import Editor from './Editor'
 
-//import '../../../style/main.css'
-
 class Main extends React.Component {
   constructor(props) {
     super(props)
