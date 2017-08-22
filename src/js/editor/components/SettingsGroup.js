78410ef99caf789b3580a96fdea7edd72751ace4
SidneyNemzer <sidney@ucode.com>
2017-08-21 22:09:53 -0700

    Allow additional classnames

--- src/js/editor/components/SettingsGroup.js
+++ src/js/editor/components/SettingsGroup.js
@@ -3,7 +3,9 @@ import React from 'react'
 import Paper from 'material-ui/Paper'
 
 const SettingsGroup = (props) => (
-  <div className="settings-group">
+  <div
+    className={'settings-group ' + (props.className || '')}
+  >
     <h2>{props.label}</h2>
     <Paper>
       {props.children}
