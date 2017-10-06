0362442e167b6fb4795ad2104b2593f212c762c7
SidneyNemzer <sidney@ucode.com>
2017-10-05 22:30:52 -0700

    Convert debounce to arrow function

--- src/js/background.js
+++ src/js/background.js
@@ -4,20 +4,23 @@ import { wrapStore } from 'react-chrome-redux'
 import { saved, saveFailed } from './editor/actions'
 import rootReducer from './editor/reducers'
 
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments
-		var later = function() {
-			timeout = null
-			if (!immediate) func.apply(context, args)
-		};
-		var callNow = immediate && !timeout
-		clearTimeout(timeout)
-		timeout = setTimeout(later, wait)
-		if (callNow)
+const debounce = (func, wait, immediate) => {
+  let timeout = null
+  return function (...args) {
+    const context = this
+    const later = () => {
+      timeout = null
+      if (!immediate) {
+        func.apply(context, args)
+      }
+    }
+    const callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) {
       func.apply(context, args)
-	}
+    }
+  }
 }
 
 /**
