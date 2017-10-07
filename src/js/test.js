1a9e0019891d2f68a5f90138bb7ecd9a9809a547
SidneyNemzer <sidney@ucode.com>
2017-10-06 20:16:47 -0700

    Cleanup

--- src/js/test.js
+++ src/js/test.js
@@ -1,5 +1,6 @@
-/* This file starts the editor to allow for interface development
-   It simulates panel.js (start editor) and background.js (redux store)
+/* This file starts the editor to allow for interface development in a
+   regular webpage. It simulates panel.js (start editor) and background.js
+   (redux store)
 */
 
 import { createStore } from 'redux'
@@ -7,52 +8,54 @@ import rootReducer from './editor/reducers'
 import { saved } from './editor/actions'
 import createEditor from './editor'
 
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments
-		var later = function() {
-			timeout = null
-			if (!immediate) func.apply(context, args)
-		};
-		var callNow = immediate && !timeout
-		clearTimeout(timeout)
-		timeout = setTimeout(later, wait)
-		if (callNow)
+const debounce = (func, wait, immediate) => {
+  let timeout = null
+  return function (...args) {
+    const context = this
+    const later = () => {
+      timeout = null
+      if (!immediate) {
+        func.apply(context, args)
+      }
+    }
+    const callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) {
       func.apply(context, args)
-	}
+    }
+  }
 }
 
-const saveStore = (store) => {
+const store = createStore(rootReducer)
+store.subscribe(debounce(() => {
   const state = store.getState()
   if (!state.saved) {
     console.log('[Noop] save store:', state)
     store.dispatch(saved())
   }
-}
-
-const store = createStore(rootReducer)
-store.subscribe(debounce(() => saveStore(store), 1500))
+}, 1500))
 
 const fakeStore = {
-	dispatch: (...args) => {
-		setTimeout(() => {
-			store.dispatch(...args)
-		}, 2)
-	}
+  dispatch: (...args) => {
+    // Delay dispatches by 2 ms to simulate the delay in react-chrome-redux
+    setTimeout(() => {
+      store.dispatch(...args)
+    }, 2)
+  }
 }
 
 /* eslint-disable no-eval */
 createEditor(eval, new Proxy(
-	store,
-	{
-		get: (target, key) => {
-			if (key === 'dispatch') {
-				return fakeStore.dispatch
-			} else {
-				return target[key]
-			}
-		}
-}))
-// Simulate react-chrome-redux store
+  store,
+  {
+    get: (target, key) => {
+      if (key === 'dispatch') {
+        return fakeStore.dispatch
+      } else {
+        return target[key]
+      }
+    }
+  }))
+// Simulate react-chrome-redux store load event
 store.dispatch({ type: 'LOADED' })
