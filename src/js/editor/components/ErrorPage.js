a603047ece5b4d2649512b3318bbf500459f51f4
SidneyNemzer <sidney@ucode.com>
2017-11-20 22:11:18 -0800

    Make the error page smarter

--- src/js/editor/components/ErrorPage.js
+++ src/js/editor/components/ErrorPage.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import Button from 'material-ui/Button'
+
+const renderError = errorText => (
+  errorText !== undefined ? <pre>{errorText}</pre> : false
+)
+
+const renderAction = (action, actionButton, onClick) => (
+  action
+    ? <p className="action">{action}</p>
+    : actionButton
+      ? <Button raised color="primary" onClick={onClick}>{actionButton}</Button>
+      : false
+)
+
+const renderLink = link => (
+  link !== undefined ? <a href={link}>{link}</a> : false
+)
 
 const ErrorPage = (props) => (
   <div className="error">
     <h1>{props.title}</h1>
     <p>{props.message}</p>
-    <pre>{props.error}</pre>
-    <p className="action">{props.action}</p>
-    <a href={props.link}>{props.link}</a>
+    {renderError(props.error)}
+    {renderAction(props.action, props.actionButton, props.onActionButtonClick)}
+    {renderLink(props.link)}
   </div>
 )
 
