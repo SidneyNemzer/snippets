6de8f3ff02d1097743ddb8ef29448060e568fff0
SidneyNemzer <sidney@ucode.com>
2017-11-26 22:47:52 -0800

    Update settings to use redux-generators

--- src/js/editor/actions/settings.js
+++ src/js/editor/actions/settings.js
@@ -1,48 +1,17 @@
-export const TAB_SIZE = 'TAB_SIZE'
-export const AUTO_COMPLETE = 'AUTO_COMPLETE'
-export const SOFT_TABS = 'SOFT_TABS'
-export const THEME = 'THEME'
-export const LINE_WRAP = 'LINE_WRAP'
-export const LINTER = 'LINTER'
-export const ACCESS_TOKEN = 'ACCESS_TOKEN'
-export const GIST_ID = 'GIST_ID'
+import { generateTypes, generateActions } from '../generate-redux'
 
-export const tabSize = size => ({
-  type: TAB_SIZE,
-  size
-})
+const types = {
+  tabSize: true,
+  autoComplete: true,
+  softTabs: true,
+  theme: true,
+  lineWrap: true,
+  linter: true,
+  accessToken: true,
+  gistId: true
+}
 
-export const autoComplete = shouldAutoComplete => ({
-  type: AUTO_COMPLETE,
-  shouldAutoComplete
-})
-
-export const softTabs = useSoftTabs => ({
-  type: SOFT_TABS,
-  useSoftTabs
-})
-
-export const theme = theme => ({
-  type: THEME,
-  theme
-})
-
-export const lineWrap = lineWrap => ({
-  type: LINE_WRAP,
-  lineWrap
-})
-
-export const linter = linter => ({
-  type: LINTER,
-  linter
-})
-
-export const accessToken = accessToken => ({
-  type: ACCESS_TOKEN,
-  accessToken
-})
-
-export const gistId = gistId => ({
-  type: GIST_ID,
-  gistId
-})
+export default {
+  types: generateTypes(types),
+  actions: generateActions(types)
+}
--- src/js/editor/reducers/settings.js
+++ src/js/editor/reducers/settings.js
@@ -1,86 +1,18 @@
-import {
-  TAB_SIZE,
-  AUTO_COMPLETE,
-  SOFT_TABS,
-  THEME,
-  LINE_WRAP,
-  LINTER,
-  ACCESS_TOKEN,
-  GIST_ID
-} from '../actions/settings'
+import { generateReducer } from '../generate-redux'
+import { types } from '../actions/settings'
 
-export const defaultState = {
-  tabSize: 2,
-  autoComplete: true,
-  softTabs: true,
-  theme: 'github',
-  lineWrap: false,
-  linter: true,
-  accessToken: false,
-  gistId: false
+const defaultState = {
+  [types.tabSize]: 2,
+  [types.autoComplete]: true,
+  [types.softTabs]: true,
+  [types.theme]: 'github',
+  [types.lineWrap]: false,
+  [types.linter]: true,
+  [types.accessToken]: false,
+  [types.gistId]: false
 }
 
-const settings = (state = defaultState, action) => {
-  switch (action.type) {
-    case TAB_SIZE:
-      return Object.assign({},
-        state,
-        {
-          tabSize: action.size
-        }
-      )
-    case AUTO_COMPLETE:
-      return Object.assign({},
-        state,
-        {
-          autoComplete: action.shouldAutoComplete
-        }
-      )
-    case SOFT_TABS:
-      return Object.assign({},
-        state,
-        {
-          softTabs: action.useSoftTabs
-        }
-      )
-    case THEME:
-      return Object.assign({},
-        state,
-        {
-          theme: action.theme
-        }
-      )
-    case LINE_WRAP:
-      return Object.assign({},
-        state,
-        {
-          lineWrap: action.lineWrap
-        }
-      )
-    case LINTER:
-      return Object.assign({},
-        state,
-        {
-          linter: action.linter
-        }
-      )
-    case ACCESS_TOKEN:
-      return Object.assign({},
-        state,
-        {
-          accessToken: action.accessToken
-        }
-      )
-    case GIST_ID:
-      return Object.assign({},
-        state,
-        {
-          gistId: action.gistId
-        }
-      )
-    default:
-      return state
-  }
+export default {
+  defaultState,
+  reducer: generateReducer(defaultState)
 }
-
-export default settings
