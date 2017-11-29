4f7562bd53121c7d1e0b8a65e0818db3e18c3c13
SidneyNemzer <sidney@ucode.com>
2017-11-28 22:36:34 -0800

    Use new settings action types

--- src/js/editor/middleware/settings.js
+++ src/js/editor/middleware/settings.js
@@ -1,35 +1,15 @@
-import {
-  TAB_SIZE,
-  AUTO_COMPLETE,
-  SOFT_TABS,
-  THEME,
-  LINE_WRAP,
-  LINTER,
-  ACCESS_TOKEN,
-  GIST_ID
-} from '../actions/settings'
+import { types } from '../actions/settings'
 
 /* This middleware handles saving settings to the current environement's
    storage. In Chrome, this is chrome.storage.sync. In the browser test page,
    it's localStorage
  */
 
-const settingActions = {
-  [TAB_SIZE]: 'size',
-  [AUTO_COMPLETE]: 'shouldAutoComplete',
-  [SOFT_TABS]: 'useSoftTabs',
-  [THEME]: 'theme',
-  [LINE_WRAP]: 'lineWrap',
-  [LINTER]: 'linter',
-  [ACCESS_TOKEN]: 'accessToken',
-  [GIST_ID]: 'gistId'
-}
-
 const isSettingAction = action =>
-  Object.keys(settingActions).includes(action.type)
+  Object.keys(types).includes(action.type)
 
 const settingActionPayload = action =>
-  action[settingActions[action.type]]
+  action[types[action.type]]
 
 export default storage => store => next => action => {
   if (isSettingAction(action)) {
