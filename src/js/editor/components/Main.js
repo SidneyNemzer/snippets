377abe47c01b4a54063716f5ff88dd364e36211e
SidneyNemzer <sidney@ucode.com>
2018-01-01 11:30:48 -0800

    Ensure newData exists before accesing it in componentWillRecieveProps

--- src/js/editor/components/Main.js
+++ src/js/editor/components/Main.js
@@ -79,12 +79,12 @@ class Main extends React.Component {
       this.setState({
         selectedSnippet: Object.keys(newData)[0]
       })
-    } else if (!newData[selectedSnippet]) {
+    } else if ( newData && !newData[selectedSnippet]) {
       if (newData[data[selectedSnippet].renamed]) {
         this.setState({
           selectedSnippet: data[selectedSnippet].renamed
         })
-      } else if (Object.keys(newData)) {
+      } else if (newData && Object.keys(newData)) {
         this.setState({
           selectedSnippet: Object.keys(newData)[0]
         })
