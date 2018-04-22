a7d9f579fe66f6e1f08fd41c26b50d4e6d70f9fc
SidneyNemzer <sidney@ucode.com>
2018-04-21 19:34:29 -0700

    Cleanup layout

--- src/js/editor/components/Welcome.js
+++ src/js/editor/components/Welcome.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'material-ui/Button'
 
 import { pages } from '../constants'
 
-const Welcome = props => (
-  <div>
+import logo from '../../../../images/logo-transparent.png'
+
+const Welcome = () => (
+  <div style={{ maxWidth: 700, margin: '10vh auto', textAlign: 'center' }}>
+    <img
+      src={logo}
+      style={{ width: '75%' }}
+    />
     <h1>Welcome to Snippets 2.0!</h1>
     <p>
-      Snippets are now stored using Github Gists, so to get started you&apos;ll need
+      Snippets are now stored in a Github Gist, so you&apos;ll need
       to authenticate with Github.
     </p>
-    <Link to={pages.LOGIN}><button>Login</button></Link>
+    <p>
+      (You can import snippets from Chrome storage in the settings, after you login)
+    </p>
+    <Link to={pages.LOGIN} style={{ textDecoration: 'none' }}>
+      <Button raised color="primary">Login</Button>
+    </Link>
   </div>
 )
 
