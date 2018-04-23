008f9981fd00dbc14056bc87328ba17b806ab5bb
SidneyNemzer <sidney@ucode.com>
2018-04-22 21:32:16 -0700

    Improve login page layout

--- src/js/editor/components/Login.js
+++ src/js/editor/components/Login.js
@@ -1,36 +1,26 @@
 import React from 'react'
-
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import Button from 'material-ui/Button'
+import TextField from 'material-ui/TextField'
 
 import { pages, CREATE_ACCESS_TOKEN_URL } from '../constants'
 import { actions as settingsActions } from '../actions/settings'
 import { loadSnippets } from '../actions/snippets'
 
-// TODO Move this to utility file
-const updater =
-  (key, context) =>
-    newValue =>
-      context.setState({
-        [key]: newValue
-      })
-
-const accessTokenUrl = 'https://github.com/settings/tokens/new?description=Snippets%20Access%20Token&scopes=gist'
-
 class Login extends React.Component {
-  constructor(props) {
-    super(props)
+  state = {
+    accessTokenInput: ''
+  }
 
-    this.state = {
-      accessTokenInput: ''
-    }
-
-    this.updateAccessTokenInput = updater('accessTokenInput', this)
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    })
   }
 
   componentWillReceiveProps({ settingsAccessToken: nextAccessToken }) {
     const { gistId, settingsAccessToken } = this.props
-    console.log('componentWillReceiveProps', nextAccessToken, settingsAccessToken)
     if (nextAccessToken && nextAccessToken !== settingsAccessToken) {
       if (gistId) {
         this.props.loadSnippets()
@@ -41,19 +31,26 @@ class Login extends React.Component {
     }
   }
 
-  submitAccessToken() {
-    this.props.accessToken(this.state.accessTokenInput)
-  }
-
   render() {
     return (
-      <div>
-        <h1>Please enter a GitHub personal access token</h1>
-        <input
+      <div style={{ maxWidth: 700, margin: '10vh auto', textAlign: 'center' }}>
+        <h1>Authenticate with Github</h1>
+        <p>
+          In order to store snippets in a Github Gist, you'll need to provide a
+          Github access token. It must have the "Gist" scope.
+        </p>
+        <TextField
+          label="Enter access token"
           value={this.state.accessTokenInput}
-          onInput={event => this.updateAccessTokenInput(event.target.value)}
+          onChange={this.handleChange('accessTokenInput')}
         />
-        <button onClick={() => this.submitAccessToken()}>Done</button>
+        <Button
+          raised
+          color="primary"
+          onClick={() => this.props.accessToken(this.state.accessTokenInput)}
+        >
+          Save
+        </Button>
         <p>You can make one <a href={CREATE_ACCESS_TOKEN_URL} target='_blank'>here</a></p>
       </div>
     )
