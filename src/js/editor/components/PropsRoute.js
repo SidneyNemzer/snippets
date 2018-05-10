b8c0811fae07136e36a74ef813224f8f3fc655f0
SidneyNemzer <sidney@ucode.com>
2018-05-09 21:20:39 -0700

    Use a custom router to pass props to Main component

---
+++ src/js/editor/components/PropsRoute.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+const renderMergedProps = (component, ...rest) => {
+  const finalProps = Object.assign({}, ...rest)
+  return (
+    React.createElement(component, finalProps)
+  )
+}
+
+const PropsRoute = ({ component, ...rest }) => (
+  <Route {...rest} render={routeProps => {
+    return renderMergedProps(component, routeProps, rest)
+  }}/>
+)
+
+export default PropsRoute
