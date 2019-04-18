91f990378a9a8d51e40559d105622285eab0bb45
SidneyNemzer <sidney@ucode.com>
2019-04-17 22:55:50 -0700

    feat: update text on welcome page

--- src/js/editor/components/Welcome.js
+++ src/js/editor/components/Welcome.js
@@ -12,16 +12,16 @@ const Welcome = () => (
       src={logo}
       style={{ width: '75%' }}
     />
-    <h1>Welcome to Snippets 2.0!</h1>
+    <h1>Welcome to Snippets!</h1>
     <p>
       Snippets are stored in a Github Gist, so you&apos;ll need
-      to authenticate with Github. <br /> (We'll use an access token to do that).
+      to authenticate with Github. <br /> (We'll use a personal access token to do that).
     </p>
     <Link to={pages.LOGIN} style={{ textDecoration: 'none' }}>
       <Button variant="contained" color="primary">Login</Button>
     </Link>
     <p style={{ color: 'gray', fontStyle: 'italic' }}>
-      Note to users Snippets before version 2.0: <br />you can import snippets from Chrome storage in the settings, after you login
+      If you used Snippets before version 2.0: <br />you can import snippets from Chrome storage in the settings, after you login
     </p>
   </div>
 )
