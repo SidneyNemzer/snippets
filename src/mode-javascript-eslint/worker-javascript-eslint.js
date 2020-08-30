a56f1826244df2d723f62a287f4dffe3c6d394e9
SidneyNemzer <SidneyNemzer@users.noreply.github.com>
2020-08-30 12:51:06 -0700

    tweak annotation types and add more envs

--- src/mode-javascript-eslint/worker-javascript-eslint.js
+++ src/mode-javascript-eslint/worker-javascript-eslint.js
@@ -11,6 +11,27 @@ import eslint from 'eslint/lib/linter/linter'
 
 const linter = new eslint.Linter()
 
+// ESLint allows rules to be 'warning' or 'error' but Ace also has 'info',
+// this object defines the type of each rule. These are subjective but
+// generally errors are more likely to indicate a problem. Rules like
+// `no-undef` normally indicate an error but Snippets are likely to depend on
+// values defined in the page.
+const ruleAnnotationType = {
+  'no-dupe-args': 'warning',
+  'no-dupe-else-if': 'warning',
+  'no-dupe-keys': 'warning',
+  'no-duplicate-case': 'warning',
+  'no-template-curly-in-string': 'warning',
+  'no-unexpected-multiline': 'error',
+  'use-isnan': 'error',
+  'valid-typeof': 'warning',
+  'no-new-wrappers': 'warning',
+  'no-undef': 'info',
+  'no-unused-vars': 'info',
+  'no-const-assign': 'error',
+  'no-dupe-class-members': 'warning'
+}
+
 ace.define('ace/mode/javascript_worker_eslint', function(require, exports, module) {
   const oop = require("ace/lib/oop");
   const Mirror = require("ace/worker/mirror").Mirror
@@ -30,20 +51,22 @@ ace.define('ace/mode/javascript_worker_eslint', function(require, exports, modul
               ecmaVersion: "2020",
               sourceType: 'script'
             },
-            env: { browser: true },
+            env: { browser: true, es6: true, es2017: true, es2020: true },
             rules: {
+              // We define the error level separately so everything is set to
+              // 'warning' here (`1`).
               'no-dupe-args': 1,
               'no-dupe-else-if': 1,
               'no-dupe-keys': 1,
               'no-duplicate-case': 1,
               'no-template-curly-in-string': 1,
-              'no-unexpected-multiline': 2,
-              'use-isnan': 2,
-              'valid-typeof': 2,
+              'no-unexpected-multiline': 1,
+              'use-isnan': 1,
+              'valid-typeof': 1,
               'no-new-wrappers': 1,
-              'no-undef': 2,
+              'no-undef': 1,
               'no-unused-vars': 1,
-              'no-const-assign': 2,
+              'no-const-assign': 1,
               'no-dupe-class-members': 1
             }
         };
@@ -72,8 +95,9 @@ ace.define('ace/mode/javascript_worker_eslint', function(require, exports, modul
                 annotations.push({
                     row: message.line - 1,
                     column: message.column - 1,
-                    text: `${message.message}\n${message.ruleId}`,
-                    type: message.severity === 1 ? 'warning' : 'error'
+                    text: `${message.message}${message.ruleId ? `\n${message.ruleId}` : ''}`,
+                    // parsing errors don't have a rule ID
+                    type: message.ruleId ? ruleAnnotationType[message.ruleId] : 'error'
                 });
             }
         } catch (error) {
