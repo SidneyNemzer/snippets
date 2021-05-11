eeb79d221b5bf07278cd186983f2ee39438a2921
SidneyNemzer <SidneyNemzer@users.noreply.github.com>
2021-05-10 21:12:22 -0700

    tweak error logging

--- src/editor/middleware/log-error.ts
+++ src/editor/middleware/log-error.ts
@@ -3,11 +3,10 @@ import { Middleware } from "redux";
 const logError: Middleware = (store) => (next) => (action) => {
   if (action.error) {
     if (action.error.context) {
-      console.error("Error: failed to " + action.error.context);
+      console.error("Failed to " + action.error.context, action.error);
     } else {
-      console.error("Error (no context)");
+      console.error(action.error);
     }
-    console.error(action.error);
   }
   next(action);
 };
